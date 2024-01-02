917a6dcf18885f94747b23fe9e17550a86d1081c
linda-wederhorn <linda.wederhorn@elekta.com>
2024-01-02 19:14:46 +0200

    3.4: puhelinluettelon backend step4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const phonebook = require('./phonebook')
+let phonebook = require('./phonebook')
 app.use(express.json())
 
 console.log(phonebook)
@@ -15,12 +15,20 @@ app.get('/api/persons', (_request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const number = phonebook.find(number => number.id === id)
-  if (number) {
-    response.json(number)
+  const person = phonebook.find(person => person.id === id)
+  if (person) {
+    response.json(person)
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  console.log("DELETE!!!")
+  const id = Number(request.params.id)
+  if (!phonebook.find(person => person.id === id)) response.status(404).end()
+  phonebook = phonebook.filter(person => person.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
