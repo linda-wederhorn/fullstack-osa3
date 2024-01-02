3fb61bcaa81b9960cb8b8ef05f85232a64754489
linda-wederhorn <linda.wederhorn@elekta.com>
2024-01-02 19:22:28 +0200

    3.5: puhelinluettelon backend step5

--- index.js
+++ index.js
@@ -31,6 +31,26 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing missing'
+    })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 10000000),
+    name: body.name,
+    number: body.number,
+  }
+
+  phonebook = phonebook.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
