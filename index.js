42a4223ad6ad010f81c566cb0dc7c1ee668b4512
linda-wederhorn <linda.wederhorn@elekta.com>
2024-01-02 19:02:51 +0200

    3.3: puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ app.use(express.json())
 
 console.log(phonebook)
 
+app.get('/info', (_request, response) => {
+  response.send('<p>Phonebook has info for ' + phonebook.length + ' people</p>' + new Date())
+})
+
 app.get('/api/persons', (_request, response) => {
   response.json(phonebook)
 })
 
-app.get('/info', (_request, response) => {
-  response.send('<p>Phonebook has info for ' + phonebook.length + ' people</p>' + new Date())
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const number = phonebook.find(number => number.id === id)
+  if (number) {
+    response.json(number)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
