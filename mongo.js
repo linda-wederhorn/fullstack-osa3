9b4337b53474aaa2bfe88e0c4ea1e5194d587542
linda-wederhorn <linda.wederhorn@elekta.com>
2024-02-13 17:05:39 +0200

    3.12: tietokanta komentoriviltä
    problems connecting to mongoDB

---
+++ mongo.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const PORT = process.env.PORT
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { useUnifiedTopology: true }, (err) => {
+  if (err) console.warn("\n\nError connecting to MongoDB:\n", err)
+  else console.log("Successfully connected to MongoDB")
+})
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+}).catch(err => console.warn("\n\nError in saving the note:\n", err))
