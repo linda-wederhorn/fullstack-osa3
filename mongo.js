c5a3431b9ae832904fa176a447ef4e065a012d61
linda-wederhorn <linda.wederhorn@elekta.com>
2024-03-05 11:25:43 +0200

    3.12: tietokanta komentoriviltä

--- mongo.js
+++ mongo.js
@@ -3,25 +3,38 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-const PORT = process.env.PORT
-const url = process.env.MONGODB_URI
+// const PORT = process.env.PORT
+// const url = process.env.MONGODB_URI
+
+const password = process.argv[2]
+const url = `mongodb+srv://lindawederhorn:${password}@linda.bsgaanv.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
+if (process.argv.length == 5) {
+  const person = new Person({
+    "name": process.argv[3],
+    "number": process.argv[4]
+  })
+  person.save().then(result => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  }).catch(err => console.warn("\n\nError in saving the note:\n", err))
+}
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-}).catch(err => console.warn("\n\nError in saving the note:\n", err))
+if (process.argv.length == 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, " ", person.number)
+    })
+    mongoose.connection.close()
+  })
+}
